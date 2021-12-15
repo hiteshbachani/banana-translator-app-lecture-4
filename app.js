c83e1d8b3370387a0d886f017b1f5b4830e5dd1e
Hitesh Bachani <hiteshvbachani@gmail.com>
2021-12-15 15:06:46 +0530

    add translate button

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("script is working from a different file.")
 
 var userName = prompt("Give me your username!");
 alert("this script works") + userName;
+
+var btnTranslate = document.querySelector("#btn-translate")
