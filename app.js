2ac1ecb4af07b7b5ef669515bd7e96b569ee42fd
Hitesh Bachani <hiteshvbachani@gmail.com>
2021-12-15 15:34:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-console.log("script is working from a different file.")
-
 // var userName = prompt("Give me your username!");
 // alert("this script works") + userName;
 
-var btnTranslate = document.querySelector("#btn-translate")
-
-function clickEventHandler(click){
-    console.log("clicked")
-}
-
-btnTranslate.addEventListener("click", clickEventHandler());
+var btnTranslate = document.querySelector("#btn-translate");
+btnTranslate.addEventListener("click", clickEventHandler);
 
 
