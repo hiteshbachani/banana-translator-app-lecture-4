58648f3b600ab9aa15d7e5ded45b176a9395702a
Hitesh Bachani <hiteshvbachani@gmail.com>
2021-12-15 22:54:24 +0530

    add output

--- app.js
+++ app.js
@@ -2,12 +2,17 @@
 // alert("this script works") + userName;
 
 var btnTranslate = document.querySelector("#btn-translate");
+var txtinput = document.querySelector("#txt-input"); 
+
 btnTranslate.addEventListener("click", clickEventHandler);
 
 function clickEventHandler(){
     console.log("clicked!");
     console.log(txtinput.value);
+    outputDivinnertext="Hitesh Bachani"
 }
 
-// var txtinput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector(#output);
+outputDivinnertext = "Hitesh Bachani";
+// 
 
